0fc03b399b8a89c548b45910783bd2990377e6ed
Jacob T Stockwell <JacobTStockwell@gmail.com>
2021-06-22 11:25:21 -0500

    Displays on click and moved the form to right

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../books'
-import { CATALOG } from '../catalog'
+import { BOOKS } from '../catalog'
 
 
 @Component({
@@ -12,16 +12,28 @@ export class BookListComponent implements OnInit {
 
   
   //somehow get the catalog array to replace this 
+
+
+  /* previous version -----------------------------------------------------------------------------------------
   books : Book = {
     id: 1,
     name: 'Ready Player One',
     description: 'A dystopia in 2045, on the search for an Easter egg in a worldwide virtual reality game.'
-  }
+  }-------------------------------------------------------------------------------------------------------- */
+
+  books = BOOKS;
+  selectedBook?: Book;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(book: Book): void {
+    this.selectedBook = book;
+  }
+  
+  //placeholder ---------------------------------------------------
   updateBook()
   {
     alert("book updated!")
@@ -30,5 +42,5 @@ export class BookListComponent implements OnInit {
   {
     alert("book deleted!")
   }
-
+  //-------------------------------------------------------------------
 }
--- src/app/catalog.ts
+++ src/app/catalog.ts
@@ -1,14 +1,14 @@
 import { Book } from './books';
 
-export const CATALOG: Book[] = [
-  { id: 1, name: 'ready Player one', description: 'a dystopia in 2045, on the search for an Easter egg in a worldwide virtual reality game' },
+export const BOOKS: Book[] = [
+  { id: 1, name: 'Ready Player One ', description: 'A dystopia in 2045, on the search for an Easter egg in a worldwide virtual reality game' },
   { id: 2, name: 'book2', description: 'something'  },
   { id: 3, name: 'book3', description: 'something'  },
   { id: 4, name: 'book4', description: 'something'  },
   { id: 5, name: 'book5', description: 'something'  },
   { id: 6, name: 'book6', description: 'something'  },
   { id: 7, name: 'book7', description: 'something'  },
-  { id: 8, name: 'Book8' , description: 'something' },
+  { id: 8, name: 'Book8', description: 'something' },
   { id: 9, name: 'book9', description: 'something'  },
   { id: 10, name: 'book10', description: 'something'  },
   { id: 11, name: 'book11', description: 'something'  },
